01b7beded0f526bbd62c4924d80c9453aaab529f
Sven Wambecq <swambecq@cochlear.com>
2019-04-13 21:44:46 +0200

    updates

---
+++ lib/Target/Neo/MCTargetDesc/NeoMCTargetDesc.h
@@ -0,0 +1,61 @@
+//===-- SparcMCTargetDesc.h - Sparc Target Descriptions ---------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file provides Sparc specific target descriptions.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_NEO_MCTARGETDESC_NEOMCTARGETDESC_H
+#define LLVM_LIB_TARGET_NEO_MCTARGETDESC_NEOMCTARGETDESC_H
+
+#include "llvm/Support/DataTypes.h"
+
+namespace llvm {
+class MCAsmBackend;
+class MCCodeEmitter;
+class MCContext;
+class MCInstrInfo;
+class MCObjectWriter;
+class MCRegisterInfo;
+class MCSubtargetInfo;
+class MCTargetOptions;
+class Target;
+class Triple;
+class StringRef;
+class raw_pwrite_stream;
+class raw_ostream;
+
+Target &getTheNeoTarget();
+Target &getTheNeoXSTarget();
+
+MCCodeEmitter *createNeoMCCodeEmitter(const MCInstrInfo &MCII,
+                                        const MCRegisterInfo &MRI,
+                                        MCContext &Ctx);
+MCAsmBackend *createNeoAsmBackend(const Target &T, const MCRegisterInfo &MRI,
+                                    const Triple &TT, StringRef CPU,
+                                    const MCTargetOptions &Options);
+MCObjectWriter *createNeoELFObjectWriter(raw_pwrite_stream &OS, bool Is64Bit,
+                                           bool IsLIttleEndian, uint8_t OSABI);
+} // End llvm namespace
+
+// Defines symbolic names for Sparc registers.  This defines a mapping from
+// register name to register number.
+//
+//#define GET_REGINFO_ENUM
+//#include "NeoGenRegisterInfo.inc"
+
+// Defines symbolic names for the Sparc instructions.
+//
+#define GET_INSTRINFO_ENUM
+#include "NeoGenInstrInfo.inc"
+
+#define GET_SUBTARGETINFO_ENUM
+#include "NeoGenSubtargetInfo.inc"
+
+#endif
